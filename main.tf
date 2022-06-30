2d5e781295ac2c554fbfeef14ff48fa79f98dd90
Josh Sinfield <joshuasinfield@googlemail.com>
2022-06-30 14:24:15 +0100

    remove .terraform-version & add vars for prefix lists and ipv6 cidrs (#9)
    * remove .terraform-version as module works with later version
    
    * feat: Add support to add prefix lists and IPv6 CIDRs to route tables
    
    Co-authored-by: Dogers <Dogers@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_route" "tgw_route_cidr" {
-  for_each               = toset(var.route_tables)
-  route_table_id         = each.key
-  destination_cidr_block = var.destination_cidr
-  transit_gateway_id     = var.transit_gateway_id
+  for_each                     = toset(var.route_tables)
+  route_table_id               = each.key
+  destination_cidr_block       = var.destination_cidr
+  destination_ipv6_cidr_block  = var.destination_cidr_ipv6
+  destination_prefix_list_id   = var.destination_prefix_list_id
+  transit_gateway_id           = var.transit_gateway_id
 }
